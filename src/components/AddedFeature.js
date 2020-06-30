da8dfce7eed7e47f1f03e7157550798fe652c78b
Kirk Tolliver <kirktolliver28@gmail.com>
2020-06-29 23:57:33 -0500

    Added functionality

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeItem } from '../actions';
 
 const AddedFeature = props => {
+
+  const removeFeature = event => {
+    event.preventDefault()
+    props.removeItem(props.feature)
+  }
+
   return (
     <li>
-      {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button className="button" onClick={removeFeature}>X</button>
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+const mapStateToProps = state => {
+  return {
+    additionalPrice: state.additionalPrice,
+    features: state.car.features
+  }
+}
+
+export default connect(mapStateToProps, { removeItem })(AddedFeature);
